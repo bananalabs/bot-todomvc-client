ae2b8a1fa79b6ab3b7c34fc5f0465b797e052a1e
Deepa Ramam <dshenoy@gmail.com>
2017-07-17 17:47:57 -0700

    update

--- src/feathersClient.js
+++
@@ -1,7 +0,0 @@
-import feathers from 'feathers-client';
-
-const host = '';
-
-export default feathers()
-    .configure(feathers.hooks())
-    .configure(feathers.rest(host).fetch(window.fetch.bind(window)))
